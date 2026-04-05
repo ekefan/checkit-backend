a7d782193c450df52a78d49fbfa733922b0277ff
Emmanuel Ebenezer <ekefan.dev@gmail.com>
2026-04-05 17:12:24 +0100

    add validation pipes for network i/o data

--- apps/user/src/main.ts
+++ apps/user/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { UserModule } from './user.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(UserModule);
-  await app.listen(process.env.port ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+  const port = process.env.PORT ?? 3000
+  await app.listen(port);
+
+  Logger.log(`Application started on port: [${port}]`)
 }
 bootstrap();
